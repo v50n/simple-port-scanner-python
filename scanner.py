339bbe347a3d5fc6b1b088a388400bf8fedeb311
vson75 <vson.ng91@gmail.com>
2022-11-07 11:19:50 +0100

    Create scanner.py

---
+++ scanner.py
@@ -0,0 +1,14 @@
+import socket
+srv_adr = input("the server adresse : ")
+
+print("scanning host : ",srv_adr)
+# scan port: default 10000
+for i in range(1,10000):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s_port = s.connect_ex((srv_adr,i))
+    if s_port == 0:
+        print("port : ",i," is Online")
+    else:
+            print("port : ",i," is closed")
+    s.close()
+                
